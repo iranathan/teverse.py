598b322596129a50417f8dc0e3acf854971c8e93
iranathan <iranathan8@gmail.com>
2019-08-17 22:05:48 +0200

    delete build file

--- build/lib/teverse/game.py
+++
@@ -1,7 +0,0 @@
-class Game:
-    def __init__(self, requests):
-        self.requests = requests
-
-    async def get_games(self) -> list:
-        r = await self.requests.get(f'https://teverse.com/api/games/')
-        return await r.json()
